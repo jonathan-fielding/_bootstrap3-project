b4c4e5846faa789214204794fae9b17152fded45
James Howell <james@mccormackmorrison.com>
2014-01-07 23:08:55 +0000

    Removed unnecessary JS from plugin script and added more styles to media queries for setting carousel item widths, etc

--- js/site/plugins.js
+++ js/site/plugins.js
@@ -6,7 +6,7 @@
             itemsPerPageOnMobile: 1,
             itemsPerPageOnTablet: 2,
             itemsPerPageOnDesktop: 3,
-            carouselItemClass: ".item"
+            carouselItemClass: ".item",
         };
 
     // The actual plugin constructor
@@ -29,14 +29,11 @@
             .register("screen and (min-width:992px)", $.proxy(this.onEnterDesktop,this))
         },
 
-    
 
         onEnterMobile: function (){
             var itemsPerPage = this.settings.itemsPerPageOnMobile,
                 deviceType = "mobile";
                 this.createCarousel(itemsPerPage, deviceType);
-
-
         },
 
         onEnterTablet: function (){
@@ -55,46 +52,27 @@
             
             //Cache all list items within, and the length of the carousel
             var $carousel = $(this.element),
-                $carouselItems = $carousel.children(this.settings.carouselItemClass),
+                $carouselItem = $(this.settings.carouselItemClass);
+                $carouselItems = $carousel.children(),
                 carouselItemsLength = $carouselItems.length,
                 carouselItemsTotalWidth = 0,
-                totalWidth = 0,
                 tallestItemHeight = 0,
                 $carouselContainer = $carousel.parent(),
-                carouselWidth = $carouselContainer.outerWidth(),
-                carouselItemWidth = carouselWidth / itemsPerPage,
-                itemsPerPage = itemsPerPage,
-                deviceType = deviceType;
 
-
-                var setupCarousel = function(){
+                setupCarousel = function(){
 
                     //Add enabled class to Carousel's containing element which will apply further styles for users with JS
                     $carouselContainer.addClass("enabled");
 
-                    for (var i = 0; i < carouselItemsLength; i++) {
-                        
-                        $carousel.children(".item").eq(i).height("auto");
-
-                        if(deviceType==="mobile"){
-                            //Don't set a width on carousel items when viewed on a mobile device. The main reason for this is the $carouselContainer does not have a max-width property set on device screens below 768px
-                            $carousel.children(".item").eq(i).css({
-                                "width": "100%",
-                            });
-                        }else{
-                            //Add a width to each item within the carousel: total width of carousel / the number of items = item width
-                            $carousel.children(".item").eq(i).outerWidth(carouselItemWidth);
-                        }
-                        
-
+                    for (var i = 0; i < carouselItemsLength; i++) {                        
                         //Reset height of list item to auto if previously set to static height
-                        $carousel.children(".item").eq(i).height("auto");
+                        $carousel.children($carouselItem).eq(i).height("auto");
                         
                         //Find the tallest item by comparing the current item's height with the previous tallest.
-                        tallestItemHeight = $carousel.children(".item").eq(i).height() > tallestItemHeight?$carousel.children(".item").eq(i).height():tallestItemHeight;
+                        tallestItemHeight = $carousel.children($carouselItem).eq(i).height() > tallestItemHeight?$carousel.children($carouselItem).eq(i).height():tallestItemHeight;
                         
                         //Get each element's width and add it to a total width of the carousel.
-                        carouselItemsTotalWidth = carouselItemsTotalWidth + $carousel.children(".item").eq(i).outerWidth();
+                        carouselItemsTotalWidth = carouselItemsTotalWidth + $carousel.children($carouselItem).eq(i).outerWidth();
                     };
 
                     //Set all elements within the carousel to the height of the tallest item
@@ -104,14 +82,18 @@
                     $carouselContainer.height(tallestItemHeight);
 
                     if(deviceType!=="mobile"){
-                        //Set the width of the carousel to the sum of the widths of its content. This will create a strip of list items all side by side.
+                        //Set the width of the carousel to the sum of the widths of its content. This will create a strip of list items all side by side. 
                         $carousel.css({"width":carouselItemsTotalWidth});
-                    }else{
+                    }
+
+                    else{
+                        //On mobile view only 1 carousel item is shown at a time. If the carousel's width exceeds the width of a single image (as it does on devices above 768px) then
+                        //the max-width property of the image is rendered useless in this view and the images will no longer be responsive.
+
+                        //So reset the width of the carousel to auto so the carousel can only ever be the width of a single image. 
                         $carousel.css({"width":"auto"});
                     }    
-
                 }();
-
 
         }
     };
